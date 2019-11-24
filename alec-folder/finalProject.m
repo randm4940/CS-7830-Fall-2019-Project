a440e78eddf155b065b6f663be9962d6fbae362a
apetrack13 <56127479+apetrack13@users.noreply.github.com>
2019-11-24 12:27:26 -0500

    Removed fluff from final project code.
    Simplified Alec's final project code.

--- alec-folder/finalProject.m
+++ alec-folder/finalProject.m
@@ -1,49 +1,23 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Wright State University                                Machine Learning %
 % Written By: Alec Petrack                 Instructor: Dr. Tanvi Banerjee %
-% Modified: 11/7/2019                                                     %
+% Modified: 11/24/2019                                                     %
 %                                                                         %
 % Code Description: Final project code for machine learning.              %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all; close all; clc
 
-%% Load Data
-X = xlsread('../processedHeart.xlsx'); % read data
+%% Load Pre-Procssed Data
+X = xlsread('../data/processedHeart.xlsx'); % read data
+numFolds = 5;
+indices = crossvalind('Kfold',X,numFolds);
 
-%% Perform PCA
+%% Analyze Features in Dataset
+% principle component analysis
 [coeff,score,latent,tsquared,explained,mu] = pca(X(:,1:end-1));
 figure('name','Relationship Between Features')
 imagesc(coeff)
 colorbar
-
-%% Pearson's R
+% pearson's R
 [rho,pval] = corr(X(:,1:end-1));
 
-%% Split Data Into Training and Testing Sets
-numFolds = 5;
-indices = crossvalind('Kfold',X,numFolds);
-
-%% Train Logistic Regression Model to Classify Patients w/ Heart Disease
-for ii = 1:numFolds
-test = (indices == ii); 
-train = ~test;
-[B,dev,stats] = mnrfit(zscore(X(train,:)),categorical(Y(train,:)));
-pValue(:,ii) = stats.p;
-yHat(:,ii) = sigmoid([ones(size(X,1),1),zscore(X)]',B);
-end
-
-%% Evaluate Performance of the Model
-% p = stats.p;
-% cp = classperf(species);
-
-%% Algorithms/Equations
-
-function hypothesis = sigmoid(X,theta)
-hypothesis = round(1./(1+exp(-theta'*X)));
-end
-
-% extra trees classifier
-
-
-% rest ecg and "slope" sort of the same (one is at rest, the other not
-% rest)
